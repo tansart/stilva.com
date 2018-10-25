c1a5d0a38ad2e9c525fb3fe60916ebc84280c4a5
Thomas <thomas.ansart@stilva.com>
2018-10-25 14:49:30 -0400

    allows for hiding tiles, cancelling, and toggling hide/show

--- src/utils/RectangleAnimation.js
+++ src/utils/RectangleAnimation.js
@@ -1,4 +1,8 @@
 export default class RectangleAnimation {
+	get time() {
+		return this._time/this.duration;
+	}
+
 	constructor(x, y, w, h) {
 		this.x = x * 255;
 		this.y = y * 255;
@@ -7,23 +11,36 @@ export default class RectangleAnimation {
 
 		this.output = null;
 
-		this.time = 0;
+		this._time = 0;
+		this._cancelled = false;
 	}
 
 	ease(r) {
 		return (r === 1 ? 1 : 1 - Math.pow(2, - 10 * r)) * 255;
 	}
 
-	animationProperties(texture, isShowAnimation, direction) {
+	update(delta) {
+		if(this._cancelled) return;
+		this._time += delta;
+	}
+
+	animationProperties(duration, texture, isHiding, direction) {
+		this.duration = duration / 1000;
 		this.texture = texture;
-		this.isShowAnimation= isShowAnimation;
+		this.isHiding = isHiding;
 		this.direction = direction;
 	}
 
 	getData() {
 		this.output = this.output || [
-			this.x, this.y, this.w, this.h,
-			this.texture, 0 /* time */, this.isShowAnimation? 0: 255, this.direction
+			0,0,0,this.x,
+			0,0,0,this.y,
+			0,0,0,this.w,
+			0,0,0,this.h,
+			0,0,0,this.texture,
+			0,0,0,0 /* time */,
+			0,0,0,this.isHiding? 255: 0,
+			0,0,0,this.direction < 0 ? 0: 255 /* -1 to left, 1 to right */
 		];
 
 		return this.output;
