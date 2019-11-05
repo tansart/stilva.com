61038892dd9f3b46f9df085d90ab777c6c1ec58e
Thomas <thomas.ansart@stilva.com>
2019-11-04 20:46:25 -0500

    copy changes

--- src/lab.js
+++ src/lab.js
@@ -29,11 +29,15 @@ export default {
     content: [
       {
         type: 'Markdown',
-        content: `In adding entries to this lab, I realised I needed an inline link component. However, that would have complicated my JSON like data structure. Instead, I decided to build a small markdown component, so I could write my articles in markdown, and publish them.
+        content: `In adding entries to this lab, my list of custom component started growing. Instead of writing custom components, I thought I'd build a small markdown component, so I could write my articles in markdown, and publish them.
 
 It's on github: [@stilva/markdown](https://www.github.com/stilva/markdown), check it out.
 
-There's a few performance tests I'd like to run:
+I haven't gotten around writing any documentation, but the test within the \`./src\` folder can serve as documentation.`
+      },
+      {
+        type: 'Markdown',
+        content: `There's a few performance tests I'd like to run:
 * What are the performance implications in creating DOM elements vs building string representations and using \`dangerouslySetInnerHTML\`
 * Same for using createElement as a building block (I'm not convinced there's much to gain by converting the input as actual react components, given the static nature of markdowns)
 * Avoiding regexp, and having a more abstract representation of the markdown. e.g. new Node(), new Leaf()
@@ -53,9 +57,13 @@ At the moment the only way to invalidate it is by passing a new key.`
         type: 'Markdown',
         content: `Writing fragment shaders is a lot of fun. Setting up everything to easily write a shader is much less so.
 
-Having had to go through the whole ceremony on more than one occasion in the last few months, I've decided to finally write a small library to ease the pain.
+Having had to go through the whole process on more than one occasion in the last few months, I've decided to finally write a small library to ease the pain.
 This is based on code from back in 2016, and I've updated it to use the template literals so it looks nicer. 
 It's on github: [@stilva/glsl](https://www.github.com/stilva/glsl), check it out.`
+      },
+      {
+        type: 'Markdown',
+        content: `The following is a quick basic shader sample:`
       },
       {
         type: 'CanvasComponent',
@@ -77,9 +85,11 @@ It's on github: [@stilva/glsl](https://www.github.com/stilva/glsl), check it out
       {
         type: 'Markdown',
         content:
-          `This library allows for a much more concise way of writing shaders.
+          `This library allows for you to focus on just the shader – a very basic shadertoy, if you may.
 
 The clearing of the buffer in the rendering loop, setting the vertex shader, uniform locations etc are all taken care of for you.
+
+The sample above was written with the following:
 
 \`\`\`javascript
 const GLSL = require('@stilva/glsl');
@@ -92,7 +102,9 @@ glsl.fragment\`void main() {
   vec3 col = 0.5 + 0.5 * cos(u_time + uv.xyx + u_delta);
   gl_FragColor = vec4(col, 1.0);
 }\`;
-glsl.render();\`\`\``
+glsl.render();\`\`\`
+
+Note how some uniforms are provided for you by default, while you're easily able to add your own, by calling \`glsl.addVariable()\``
       },
       {
         type: 'Markdown',
